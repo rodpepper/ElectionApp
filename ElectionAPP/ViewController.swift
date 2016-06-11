b3b917bd742acf1b4b317f897f907951e2ae8d13
rodpepper <rodpepper@gmail.com>
2016-06-11 11:03:55 -0300

    Initial Commit

---
+++ ElectionAPP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ElectionAPP
+//
+//  Created by peppermini on 6/11/16.
+//  Copyright © 2016 RMPM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
